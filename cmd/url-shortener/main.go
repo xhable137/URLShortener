8c4fd5cca0920a1fc2ddf2c20e57d46c2b70ebd0
xhable137 <mr.adarmanyan@mail.ru>
2025-08-12 14:22:43 +0300

    All fixed, postres was added

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"URLShortener/iternal/config"
+	"URLShortener/internal/config"
+	"URLShortener/internal/http-server/handlers/redirect"
+	"URLShortener/internal/http-server/handlers/url/save"
+	"URLShortener/internal/lib/logger/handlers/slogpretty"
+	"URLShortener/internal/lib/storage"
+	"context"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -20,9 +32,61 @@ func main() {
 	log.Info("Starting URL Shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	// TODO: init router
+	// Initialize PostgreSQL storage
+	postgresStorage, err := storage.NewPostgresStorage(cfg)
+	if err != nil {
+		log.Error("Failed to initialize PostgreSQL storage", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	defer postgresStorage.Close()
 
-	// TODO: run server
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.NewPostgresStorage(log, postgresStorage))
+	})
+
+	router.Get("/{alias}", redirect.New(log, postgresStorage))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started", slog.String("address", srv.Addr))
+	<-done
+	log.Info("shutting down server", slog.String("address", srv.Addr))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to shutdown server", slog.String("error", err.Error()))
+		return
+	}
+	log.Info("server stopped", slog.String("address", srv.Addr))
 }
 
 func setupLogger(env string) *slog.Logger {
@@ -44,4 +108,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,25 @@ module URLShortener
 
 go 1.24
 
-require github.com/ilyakaznacheev/cleanenv v1.5.0
+require (
+	github.com/fatih/color v1.18.0
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-chi/render v1.0.3
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
